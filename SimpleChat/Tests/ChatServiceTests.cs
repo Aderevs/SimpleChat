abb289400a1f64dcd6137bb5f0fd159880614685
Aderevs <aderevs@gmail.com>
2024-07-14 02:29:44 +0300

    implemented integraion tests

--- SimpleChat/Tests/ChatServiceTests.cs
+++ SimpleChat/Tests/ChatServiceTests.cs
@@ -1,143 +1,12 @@
-﻿/*using NUnit.Framework;
-using Moq;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
-using SimpleChat.DbLogic.Entities;
-using SimpleChat.DbLogic.Repositories;
-using SimpleChat.DbLogic;
-using SimpleChat.DTOs;
-using SimpleChat.Services;
-
-public class ChatServiceTests
-{
-    private Mock<IMapper> _mockMapper;
-    private Mock<IChatsRepository> _mockChatsRepository;
-    private Mock<IUsersRepository> _mockUsersRepository;
-    private Mock<IMessagesRepository> _mockMessagesRepository;
-    private Mock<ChatDbContext> _mockDbContext;
-    private ChatService _chatService;
-
-    [SetUp]
-    public void SetUp()
-    {
-        _mockMapper = new Mock<IMapper>();
-        _mockChatsRepository = new Mock<IChatsRepository>();
-        _mockUsersRepository = new Mock<IUsersRepository>();
-        _mockMessagesRepository = new Mock<IMessagesRepository>();
-        _mockDbContext = new Mock<ChatDbContext>();
-
-        _chatService = new ChatService(
-            _mockMapper.Object,
-            _mockChatsRepository.Object,
-            _mockUsersRepository.Object,
-            _mockDbContext.Object);
-    }
-
-    [Test]
-    public async Task GetAllChats_ReturnsMappedChats()
-    {
-        // Arrange
-        var chats = new List<Chat> { new Chat { ChatId = 1, Name = "Test Chat" } };
-        _mockChatsRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(chats);
-        _mockMapper.Setup(m => m.Map<IEnumerable<Chat>>(It.IsAny<IEnumerable<Chat>>())).Returns(chats);
-
-        // Act
-        var result = await _chatService.GetAllChats();
-
-        // Assert
-        Assert.That(chats, Is.EqualTo(result));
-    }
-
-    [Test]
-    public void GetAllChatsOfUserById_UserNotFound_ThrowsArgumentException()
-    {
-        // Arrange
-        _mockUsersRepository.Setup(repo => repo.GetByIdIncludeChatsConnectedToOrDefaultAsync(It.IsAny<int>())).ReturnsAsync((User)null);
-
-        // Act & Assert
-        var ex = Assert.ThrowsAsync<ArgumentException>(() => _chatService.GetAllChatsOfUserById(1));
-        Assert.That(ex.Message, Is.EqualTo("User with this ID was not found"));
-    }
-
-    [Test]
-    public async Task CreateChat_ValidChat_ReturnsChatDTO()
-    {
-        // Arrange
-        var chatDto = new ChatDTO 
-        {
-            ChatId = 1, 
-            HostUser = new UserDTO
-            {
-                UserId = 1,
-                NickName = "Name",
-            }
-        };
-        var chat = new Chat { ChatId = 1, HostUserId = 1, HostUser = new User { UserId = 1 } };
-        _mockChatsRepository.Setup(repo => repo.CheckIfChatWithSuchIdExistsAsync(It.IsAny<int>())).ReturnsAsync(false);
-        _mockUsersRepository.Setup(repo => repo.GetByIdOrDefaultAsync(It.IsAny<int>())).ReturnsAsync(chat.HostUser);
-        _mockChatsRepository.Setup(repo => repo.AddAsync(It.IsAny<Chat>())).ReturnsAsync(chat);
-        _mockMapper.Setup(m => m.Map<Chat>(It.IsAny<ChatDTO>())).Returns(chat);
-        _mockMapper.Setup(m => m.Map<ChatDTO>(It.IsAny<Chat>())).Returns(chatDto);
-
-        // Act
-        var result = await _chatService.CreateChat(chatDto);
-
-        // Assert
-        Assert.That(chatDto, Is.EqualTo(result));
-    }
-
-    [Test]
-    public void DeleteChatAndDisconnectUsersViaTransactionAsync_ChatNotFound_ThrowsArgumentException()
-    {
-        // Arrange
-        _mockChatsRepository.Setup(repo => repo.GetByIdOrDefaultAsync(It.IsAny<int>())).ReturnsAsync((Chat)null);
-
-        // Act & Assert
-        var ex = Assert.ThrowsAsync<ArgumentException>(() => _chatService.DeleteChatAndDisconnectUsersViaTransactionAsync(1, 1));
-        Assert.That(ex.Message, Is.EqualTo("Chat with this ID was not found"));
-    }
-
-    [Test]
-    public void ConnectUserToChat_ChatNotFound_ThrowsArgumentException()
-    {
-        // Arrange
-        _mockChatsRepository.Setup(repo => repo.GetByIdIncludeUsersInvolvedOrDefaultAsync(It.IsAny<int>())).ReturnsAsync((Chat)null);
-
-        // Act & Assert
-        var ex = Assert.ThrowsAsync<ArgumentException>(() => _chatService.ConnectUserToChat(1, 1));
-        Assert.That(ex.Message, Is.EqualTo("Chat 7with this ID was not found"));
-    }
-
-    [Test]
-    public void DisconnectUserFromChat_ChatNotFound_ThrowsArgumentException()
-    {
-        // Arrange
-        _mockChatsRepository.Setup(repo => repo.GetByIdIncludeUsersInvolvedOrDefaultAsync(It.IsAny<int>())).ReturnsAsync((Chat)null);
-
-        // Act & Assert
-        var ex = Assert.ThrowsAsync<ArgumentException>(() => _chatService.DisconnectUserFromChat(1, 1));
-        Assert.That(ex.Message, Is.EqualTo("Chat with this ID was not found"));
-    }
-}
-
-*/
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Moq;
 using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using SimpleChat;
 using SimpleChat.DTOs;
 using Microsoft.EntityFrameworkCore;
 using SimpleChat.DbLogic.Entities;
 using SimpleChat.DbLogic.Repositories;
 using SimpleChat.DbLogic;
 using SimpleChat.Services;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace YourNamespace.Tests
 {
@@ -158,7 +27,7 @@ namespace YourNamespace.Tests
             _usersRepositoryMock = new Mock<IUsersRepository>();
             _dbContextOptions = new DbContextOptionsBuilder<ChatDbContext>()
                             .UseInMemoryDatabase(databaseName: "ChatDatabase")
-            .Options;   
+            .Options;
 
             _dbContext = new ChatDbContext(_dbContextOptions);
             _mapperMock = new Mock<IMapper>();
---
+++ SimpleChat/Tests/MessageServiceTests.cs
@@ -0,0 +1,192 @@
+﻿using NUnit.Framework;
+using AutoMapper;
+using SimpleChat.DbLogic.Entities;
+using SimpleChat.DbLogic.Repositories;
+using SimpleChat.DTOs;
+using SimpleChat.Services;
+using SimpleChat.DbLogic;
+
+namespace YourNamespace.Tests
+{
+    public class MessageServiceIntegrationTests
+    {
+        private ChatDbContext _context;
+        private IMapper _mapper;
+        private IMessagesRepository _messagesRepository;
+        private IChatsRepository _chatsRepository;
+        private IUsersRepository _usersRepository;
+        private MessageService _messageService;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            string connectionString = "";//Enter here your connection string for testing db
+
+            _context = new ChatDbContext(connectionString);
+            await _context.Database.EnsureCreatedAsync();
+            _mapper = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Message, MessageDTO>().ReverseMap();
+                cfg.CreateMap<Chat, ChatDTO>().ReverseMap();
+                cfg.CreateMap<User, UserDTO>().ReverseMap();
+            }).CreateMapper();
+
+            _messagesRepository = new MessagesRepository(_context);
+            _chatsRepository = new ChatsRepository(_context);
+            _usersRepository = new UsersRepository(_context);
+            _messageService = new MessageService(_mapper, _messagesRepository, _chatsRepository, _usersRepository);
+
+            await SeedDatabase();
+        }
+
+        private async Task SeedDatabase()
+        {
+            var chat = new Chat { ChatId = 1, Name = "test chat", HostUserId = 1 };
+            var user = new User { UserId = 1, NickName = "test user", ChatsConnectedTo = new List<Chat> { chat } };
+            var user2 = new User { UserId = 2, NickName = "test user2" };
+            _context.Users.Add(user);
+            _context.Users.Add(user2);
+            _context.Chats.Add(chat);
+            await _context.SaveChangesAsync();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+
+        [Test]
+        public async Task CreateMessage_ChatAndUserExist_MessageCreated()
+        {
+            // Arrange
+            var messageDTO = new MessageDTO { ChatId = 1, UserId = 1, Content = "Hello" };
+
+            // Act
+            var result = await _messageService.CreateMessage(messageDTO);
+
+            // Assert
+            Assert.That("Hello", Is.EqualTo(result.Content));
+            Assert.That(1, Is.EqualTo(result.ChatId));
+            Assert.That(1, Is.EqualTo(result.UserId));
+
+        }
+
+        [Test]
+        public void CreateMessage_ChatDoesNotExist_ThrowsArgumentException()
+        {
+            // Arrange
+            var messageDTO = new MessageDTO { ChatId = 999, UserId = 1, Content = "Hello" };
+
+            // Act & Assert
+            var ex = Assert.ThrowsAsync<ArgumentException>(async () => await _messageService.CreateMessage(messageDTO));
+            Assert.That(ex.Message, Is.EqualTo("No chat was found with an identifier matching this message field Chat.ChatId"));
+        }
+
+        [Test]
+        public void CreateMessage_UserDoesNotExist_ThrowsArgumentException()
+        {
+            // Arrange
+            var messageDTO = new MessageDTO { ChatId = 1, UserId = 999, Content = "Hello" };
+
+            // Act & Assert
+            var ex = Assert.ThrowsAsync<ArgumentException>(async () => await _messageService.CreateMessage(messageDTO));
+            Assert.That(ex.Message, Is.EqualTo("No user was found with an identifier matching this message field User.UserId"));
+        }
+
+        [Test]
+        public async Task GetAllMessagesOfChat_ChatExists_ReturnsMessages()
+        {
+            // Arrange
+            var message = new Message { ChatId = 1, UserId = 1, Content = "Hello" };
+            await _messagesRepository.AddAsync(message);
+
+            // Act
+            var result = await _messageService.GetAllMessagesOfChat(1);
+
+            // Assert
+            Assert.That(1, Is.EqualTo(result.Count()));
+            Assert.That("Hello", Is.EqualTo(result.First().Content));
+        }
+
+        [Test]
+        public void GetAllMessagesOfChat_ChatDoesNotExist_ThrowsArgumentException()
+        {
+            // Act & Assert
+            var ex = Assert.ThrowsAsync<ArgumentException>(() => _messageService.GetAllMessagesOfChat(999));
+            Assert.That(ex.Message, Is.EqualTo("Chat with this ID was not found"));
+        }
+
+        [Test]
+        public async Task ChangeMessageText_MessageExists_ChangesText()
+        {
+            // Arrange
+            var message = new Message { ChatId = 1, UserId = 1, Content = "Old Content" };
+            await _messagesRepository.AddAsync(message);
+
+            // Act
+            var result = await _messageService.ChangeMessageText(message.MessageId, "New Content", 1);
+
+            // Assert
+            Assert.That("New Content", Is.EqualTo(result.Content));
+        }
+
+        [Test]
+        public void ChangeMessageText_MessageDoesNotExist_ThrowsArgumentException()
+        {
+            // Act & Assert
+            var ex = Assert.ThrowsAsync<ArgumentException>(() => _messageService.ChangeMessageText(999, "New Content", 1));
+            Assert.That(ex.Message, Is.EqualTo("Message with this ID was not found"));
+        }
+
+        [Test]
+        public async Task ChangeMessageText_UserNotAuthor_ThrowsUnauthorizedAccessException()
+        {
+            // Arrange
+            var message = new Message { ChatId = 1, UserId = 2, Content = "Old Content" };
+            await _messagesRepository.AddAsync(message);
+
+            // Act & Assert
+            var ex = Assert.ThrowsAsync<UnauthorizedAccessException>(async () => await _messageService.ChangeMessageText(message.MessageId, "New Content", 1));
+            Assert.That(ex.Message, Is.EqualTo("Only author of the message can edit it"));
+        }
+
+        [Test]
+        public async Task DeleteMessage_MessageExistsAndUserAuthorized_MessageDeleted()
+        {
+            // Arrange
+            var message = new Message { ChatId = 1, UserId = 1, Content = "Hello" };
+            await _messagesRepository.AddAsync(message);
+
+            // Act
+            await _messageService.DeleteMessage(message.MessageId, 1);
+
+            
+            // Assert
+            var deletedMessage = await _context.Messages.FindAsync(message.MessageId);
+            Assert.That(deletedMessage, Is.Null);
+        }
+
+        [Test]
+        public void DeleteMessage_MessageDoesNotExist_ThrowsArgumentException()
+        {
+            // Act & Assert
+            var ex = Assert.ThrowsAsync<ArgumentException>(async () => await _messageService.DeleteMessage(999, 1));
+            Assert.That(ex.Message, Is.EqualTo("Message with this ID was not found"));
+        }
+
+        [Test]
+        public async Task DeleteMessage_UserNotAuthorized_ThrowsUnauthorizedAccessException()
+        {
+            // Arrange
+            var message = new Message { ChatId = 1, UserId = 1, Content = "Hello" };
+            await _messagesRepository.AddAsync(message);
+
+            // Act & Assert
+            var ex = Assert.ThrowsAsync<UnauthorizedAccessException>(async () => await _messageService.DeleteMessage(message.MessageId, 2));
+            
+            Assert.That(ex.Message, Is.EqualTo("Only member of the chat can delete messages from this chat"));
+        }
+    }
+}
